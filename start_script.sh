2f523e2d814db2dc4c547916ed4e763ccc9257e4
Bharati <bharatithawait@gmail.com>
2022-02-12 15:40:02 +0530

    echo file..

--- start_script.sh
+++ start_script.sh
@@ -1,9 +1,13 @@
 echo "Hello World!"
 =======
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 echo "Welcome"
 =======
 echo "India"
+=======
+echo "Welcome"
+>>>>>>> Stashed changes
 =======
 echo "Welcome"
 >>>>>>> Stashed changes
