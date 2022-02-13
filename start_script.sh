2f76e76db5dfda95a55a77847156d8ab6e64a104
bharathi <bha062@hint90.com>
2022-02-13 13:00:22 +0530

    echo_file

--- start_script.sh
+++ start_script.sh
@@ -1,13 +1,6 @@
 echo "Hello World!"
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 echo "Welcome"
-=======
-echo "India"
-=======
-echo "Welcome"
->>>>>>> Stashed changes
-=======
-echo "Welcome"
->>>>>>> Stashed changes
+
+//"The echo is used to display the output of parameters that are passed to it. It displays the outputs of one or more strings separated by commas. The print accepts one argument at a time & cannot be used as a variable function in PHP. The print outputs only the strings."
+
